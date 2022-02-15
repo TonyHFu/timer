a005bd1ee85125faf79cdab2cfbda42bc2fcb7ff
tofutigerz <hfu3@ualberta.ca>
2022-02-15 16:41:42 -0700

    playing around with visual output using stdout

--- timer1.js
+++ timer1.js
@@ -8,15 +8,18 @@ args = args.filter(a=>{
 args.forEach(a=>{
   setTimeout(()=>{
     process.stdout.write('\x07');
-    // process.stdout.write('\x07');
-  }, a * 1000)
+    process.stdout.write('---beep');
+  }, a * 1000);
+  setTimeout(()=>{
+    process.stdout.write('       ');
+  }, a * 1000 + 1000);
 });
 
 let maxCount = Math.max(...args);
 
 for (let count = 0; count <= maxCount; count++) {
   setTimeout(() => {
-    process.stdout.write('\r' + count.toString() + '         ');
+    process.stdout.write('\r' + count.toString());
     // console.log(count);
     // console.log(counter);
     // console.log(spin[counter]);
@@ -24,7 +27,7 @@ for (let count = 0; count <= maxCount; count++) {
   }, count * 1000);
 }
 setTimeout(()=>{
-  process.stdout.write("\n");
-}, (maxCount) * 1000);
+  process.stdout.write("\rDone                    \n");
+}, (maxCount + 2) * 1000);
 
 
